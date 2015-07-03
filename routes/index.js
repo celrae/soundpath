ec330a19d95b39a4dc0dcd028675fc815f70620f
Luke Van Horn <lukevanhorn@gmail.com>
2015-07-03 18:52:47 -0400

    csv changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var ejs = require('ejs');
 var pmp = require('../services/pmp');
+var moment = require('moment-timezone');
 
 /* GET home page. */
 router.get('/search', function(req, res, next) {
@@ -39,23 +40,26 @@ router.post('/testsearch', function (req, res, next) {
         //    results.push(obj);
         //});
         
-        var csv = 'title,created,link,duration\r\n';
-
         
-        var resp = "</br><b>title&nbsp;created&nbsp;duration&nbsp;link</b><hr>";
+        var resp = '<table cellspacing="5"><th><tr><td>title</td><td>created</td><td>duration</td><td>link</td></tr></th><tbody>';
         results.forEach(function (item) {
-            resp += item.attributes.title + '&nbsp;' + item.attributes.created + '&nbsp;';
+
             item.items.forEach(function (subitem) {
                 if (subitem.links && subitem.links.enclosure 
                     && subitem.links.enclosure[0] 
                     && subitem.links.enclosure[0].type 
-                    && subitem.links.enclosure[0].type === "audio/m3u") {
-                        resp += '&nbsp;' + subitem.links.enclosure[0].meta.duration + "&nbsp;<a href='" + item.links.alternate[0].href + "'>" + item.attributes.title + "</a>";
+                    && subitem.links.enclosure[0].type === "audio/m3u" 
+                    && subitem.links.enclosure[0].href 
+                    && subitem.links.enclosure[0].meta.duration) {
+                    resp += '<tr><td>' + item.attributes.title  
+                         + '</td><td>' + moment(item.attributes.created).format("YYYY-MM-DD HH:mm:ssZ") 
+                         + '</td><td>' + subitem.links.enclosure[0].meta.duration 
+                         + "</td><td><a href='" + item.links.alternate[0].href + "'>" + item.attributes.title + "</a></td></tr>";
                 }
             });
-
-            resp += "</br>";
         });
+        
+        resp += "</tbody></table>";
         
         return res.send(resp);
        //return res.render('stories', { list: results });
